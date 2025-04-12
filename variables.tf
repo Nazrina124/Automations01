a72dbf1534ba00a10c7257da3169389818981aec
Nazrina <nazrinakhan88@gmail.com>
2025-04-12 01:39:17 -0400

    terraform vault

--- variables.tf
+++ variables.tf
@@ -1,6 +1,15 @@
 variable "tools" {
     default = {
 
+  github-runner= {
+        instance_type ="t2.micro"
+        policy_name = [
+         "AdministratorAccess"
+       ]
+       ports = {}
+     }
+    
+
  
     vault= {
       instance_type ="t2.micro"
