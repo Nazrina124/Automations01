3e4c211b2d5713051ac167962f6488428e705c74
Nazrina <nazrinakhan88@gmail.com>
2025-05-03 22:25:24 -0400

    minikube

--- variables.tf
+++ variables.tf
@@ -7,6 +7,7 @@ variable "tools" {
          "AdministratorAccess"
        ]
        ports = {}
+       volume_size = 20
      }
     
 
@@ -17,11 +18,22 @@ variable "tools" {
       ports = {
         vault = 8200
      }
+     volume_size = 20
     }
-  }
- }
 
+
+    minikube = {
+     instance_type = "t3.medium"
+     policy_name   = []
+       ports = {
+       kube= 8443
+     }
+       volume_size = 30
+      }
+  }
+}
  variable "hosted_zone_id" {
     default = "Z03035903COPLEGQ5980F"
-}
+ }
+
 
